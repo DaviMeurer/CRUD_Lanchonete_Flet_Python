f300d4713a636a48f75829a03849898485571d46
DaviMeurer <seuemail@dominio.com>
2024-12-11 20:02:25 -0400

    ignore pycache

--- main.py
+++ main.py
@@ -3,6 +3,8 @@ from views.produto_view import produto_view
 
 def main(pg:ft.Page):
     pg.title = "Restaurante Siri Cascudo"
+    pg.window_width=700
+    pg.window_height=700
     pg.horizontal_alignment=ft.CrossAxisAlignment.CENTER
     pg.auto_scroll = ft.ScrollMode.AUTO
 
